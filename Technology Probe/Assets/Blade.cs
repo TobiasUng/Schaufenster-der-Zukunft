abb403c33e2ab02be07f160ac9a5409a1877f7fb
TobiasUng <44364683+TobiasUng@users.noreply.github.com>
2022-05-02 21:21:21 +0200

    bla

--- Technology Probe/Assets/Blade.cs
+++ Technology Probe/Assets/Blade.cs
@@ -1,3 +1,6 @@
+
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -44,6 +47,98 @@ public class Blade : MonoBehaviour
 			UpdateCut();
 		}
 
+		UpdateCut();
+	}
+
+	void UpdateCut()
+	{
+		Vector2 newPosition = new Vector2(transform.position.x, transform.position.y);
+		rb.position = newPosition;
+
+		velocity = (newPosition - previousPosition).magnitude / Time.deltaTime;
+		if (velocity > minCuttingVelocity)
+		{
+			circleCollider.enabled = true;
+		}
+		else
+		{
+			//circleCollider.enabled = false;
+		}
+
+		previousPosition = newPosition;
+	}
+
+	void StartCutting()
+	{
+		isCutting = true;
+		currentBladeTrail = Instantiate(bladeTrailPrefab, transform);
+		previousPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+		circleCollider.enabled = false;
+	}
+
+	void StopCutting()
+	{
+		isCutting = false;
+		currentBladeTrail.transform.SetParent(null);
+		Destroy(currentBladeTrail, 2f);
+		circleCollider.enabled = false;
+	}
+
+	public void sliceNotice()
+    {
+		print("sliced");
+    }
+
+}
+
+
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+
+	public GameObject bladeTrailPrefab;
+	public float minCuttingVelocity = 1.0f;
+
+	bool isCutting = false;
+
+	Vector2 previousPosition;
+
+	GameObject currentBladeTrail;
+
+	Rigidbody2D rb;
+	Camera cam;
+	CircleCollider2D circleCollider;
+
+	public float velocity;
+
+	void Start()
+	{
+		cam = Camera.main;
+		rb = GetComponent<Rigidbody2D>();
+		circleCollider = GetComponent<CircleCollider2D>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			StartCutting();
+		}
+		else if (Input.GetMouseButtonUp(0))
+		{
+			StopCutting();
+		}
+
+		if (isCutting)
+		{
+			UpdateCut();
+		}
+
+		UpdateCut();
 	}
 
 	void UpdateCut()
@@ -85,4 +180,4 @@ public class Blade : MonoBehaviour
 		print("sliced");
     }
 
-}
+}*/
